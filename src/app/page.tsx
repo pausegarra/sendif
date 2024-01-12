fd88d4b7caeba50d04dbab444e64cb79ed382bdc
psegarra <pausegarra.b@gmail.com>
2024-01-12 22:14:06 +0100

    🎨 responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,18 @@ import { ReturnForm } from '@/components/return-form';
 
 export default function Home() {
   return (
-    <div className='h-screen flex justify-evenly'>
-      <div className='mt-5 max-w-[400px]'>
-        <h2 className=' text-2xl font-bold mb-5'>Itinerarios CIRCULARES / IDA Y VUELTA</h2>
+    <div className='flex flex-col md:flex-row justify-evenly items-center md:items-start h-screen'>
+      <div className='mt-5 w-full max-w-[400px] px-4 md:px-0'>
+        <h2 className='text-2xl font-bold mb-5'>Itinerarios CIRCULARES / IDA Y VUELTA</h2>
         <ReturnForm />
       </div>
 
-      <div className="w-px bg-gray-300 self-stretch mx-4" />
+      <div className="hidden md:block w-px bg-gray-300 self-stretch mx-4" />
 
-      <div className='mt-5 max-w-[400px]'>
+      <div className='mt-5 w-full max-w-[400px] px-4 md:px-0'>
         <h2 className='text-2xl font-bold mb-5'>Itinerarios LINEALES / POR ETAPAS</h2>
         <LinealForm />
       </div>
     </div>
-  )
+  );
 }
